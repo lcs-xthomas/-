3308ac8fa7fa57fcf2bae7a00385a061d8029817
Xavier Thomas-Evelyn <XThomasEvelyn@lcs.on.ca>
2026-02-08 11:20:09 -0500

    Initial Commit

---
+++ BlockLayout/BlockLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockLayoutApp.swift
+//  BlockLayout
+//
+//  Created by Zave T on 2026-02-08.
+//
+
+import SwiftUI
+
+@main
+struct BlockLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
